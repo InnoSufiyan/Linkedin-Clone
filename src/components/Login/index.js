6850b17de347eb24d696e71db1fdb140062b3ad5
inno sufiyan <innosufiyan@gmail.com>
2021-12-16 20:06:27 -0800

    17 december

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
 
 function Login(props) {
     return (
@@ -33,7 +34,16 @@ function Login(props) {
     )
 }
 
-export default Login
+
+const mapStateToProps = (state) => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
 
 const Container = styled.div`
 
